1414b8d89b3003452b5af29584653539a26e8c28
sjun <sammanone@naver.com>
2023-10-16 19:08:59 +0900

    check

--- 문자열리스트/02.py
+++ 문자열리스트/02.py
@@ -1,4 +1,24 @@
-arr = [['A', 'B', 'V', 'D', 'E'], ['Y', 'X', 'Z', 'D', 'E']]
+# arr = [['A', 'B', 'V', 'D', 'E'], ['Y', 'X', 'Z', 'D', 'E']]
+# arr = [['A', 'B', 'C', 'D', 'E']]
 
-arr2 = []
-print([row[:2] for row in arr])
+# print([row[:2] for row in arr])
+
+
+
+
+# lst = [['A', 'B'], ['Y', 'X']]
+# lst = [['A', 'B']]
+
+# unique_alphabets = set()
+# for row in lst:
+#     for char in row:
+#         print(char)
+#         unique_alphabets.add(char)
+# print(len(unique_alphabets))
+
+
+m = 2048
+arr = [-2048, 2048]
+
+if m % 2 == 0 and -m in arr and m in arr:
+    print(231)
