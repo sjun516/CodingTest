08d788806c5d8bcb25f4fcb6f1bf314d99b85a1e
sjun <sammanone@naver.com>
2023-10-21 01:43:34 +0900

    check

--- 문자열리스트/02.py
+++ 문자열리스트/02.py
@@ -6,14 +6,14 @@
 
 
 
-lst = [['A', 'B'], ['Y', 'X']]
-lst = [['A', 'B']]
+# lst = [['A', 'B'], ['Y', 'X']]
+# lst = [['A', 'B']]
 
-unique_alphabets = set()
-for row in lst:
-    for char in row:
-        print(char)
-        unique_alphabets.add(char)
-print(len(unique_alphabets))
+# unique_alphabets = set()
+# for row in lst:
+#     for char in row:
+#         print(char)
+#         unique_alphabets.add(char)
+# print(len(unique_alphabets))
 
 
